290de16516650bd7626c9b68b2a58ac37d18104c
hitode909 <hitode909@gmail.com>
2016-03-29 12:02:06 +0900

    rename

--- validator-protoascii-to-json.rb
+++ validator-protoascii-to-json.rb
@@ -1,10 +1,10 @@
 system "ruby-protoc amphtml/validator/validator.proto"
 require './amphtml/validator/validator.pb.rb'
+require 'json'
 
 protoascii = open('amphtml/validator/validator.protoascii').read.force_encoding('ASCII').scrub
 
 rules = Amp::Validator::ValidatorRules.parse_from_text(protoascii)
 rules.attr_lists
 
-require 'pp'
-pp rules.to_hash
+puts rules.to_hash.to_json
