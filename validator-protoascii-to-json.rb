9df7b1152e9fbb698176dabf07e22a24acacd9f4
hogashi <hogextend@gmail.com>
2019-05-14 16:32:03 +0900

    collect all of extension-rules

--- validator-protoascii-to-json.rb
+++ validator-protoascii-to-json.rb
@@ -2,8 +2,14 @@ system "ruby-protoc amphtml/validator/validator.proto"
 require './amphtml/validator/validator.pb.rb'
 require 'json'
 
-protoascii = open('amphtml/validator/validator-main.protoascii').read.force_encoding('ASCII').scrub
-rules = Amp::Validator::ValidatorRules.parse_from_text(protoascii)
-rules.attr_lists
+# collect all of validation rules
+protoascii_filenames = ['amphtml/validator/validator-main.protoascii'] + Dir.glob('amphtml/extensions/*/validator-*.protoascii')
 
+protoascii = ''
+protoascii_filenames.each {|file|
+  protoascii += open(file).read.force_encoding('ASCII').scrub
+}
+
+rules = Amp::Validator::ValidatorRules.parse_from_text(protoascii)
 puts rules.to_hash.to_json
+# open('validation_rules.json', 'w', 0644).write(rules.to_hash.to_json)
