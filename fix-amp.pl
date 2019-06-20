a2fa67b1e57a95e2277fcc36e7e70751288c19dd
hogashi <hogextend@gmail.com>
2019-06-20 13:01:36 +0900

    skip script tag

--- fix-amp.pl
+++ fix-amp.pl
@@ -215,6 +215,7 @@ package AmpFilter {
         my $defs = $self->validation_rules->{tags};
         for my $def (@$defs) {
             next if $def->{mandatory_parent}; # skip because we are working on blog article, not whole html
+            next if $def->{tag_name} eq 'SCRIPT'; # skip because we are working on blog article, not whole html
             $names->{$def->{tag_name}} = 1;
         }
 
